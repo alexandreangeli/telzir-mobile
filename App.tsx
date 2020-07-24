58d6cbe6b40f4cfbda4701f3e23a7296cce9c1ea
ale.angeli@hotmail.com <ale.angeli@hotmail.com>
2020-07-24 00:23:22 -0300

    splashscreen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Routes from './src/routes';
+import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import {colors} from './src/styleVariables';
 
 export default function App() {
+  useEffect(() => {
+    StatusBar.setBackgroundColor(colors.darkBlue);
+    SplashScreen.hide();
+  }, []);
+
   return <Routes />;
 }
